ddc18cbd5671efa28da0cf203814ea320a87218f
cromwel <cromwellindy@gmail.com>
2019-08-28 23:07:24 +0300

    initial commit

---
+++ Employee/Class1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    public class Employee
+    {
+        string empId;
+        string managerId;
+        int salary;
+        int ceoCount = 0;
+
+        //Dictionary to store managers and employees 
+        Dictionary<string, List<string>> managers = new Dictionary<string, List<string>>();
+        Dictionary<string, Employee> employees = new Dictionary<string, Employee>();
+
+
+        //Default constructor for Employer
+        public Employee()
+        {
+
+        }
+
+        //Employee constructor that takes the csv string
+        public Employee(string csvString)
+        {
+            validateCsv(csvString);
+        }
+
+        //Method to validate csv string
+        public string validateCsv(String csvString)
+        {
+            string result = "Fail";
+            StringReader stringReader = new StringReader(csvString);
+
+            // Given the CSV is in the format employeeId, managerId, salary
+            while (stringReader.Peek() > -1)
+            {
+                String row = stringReader.ReadLine();
+                String[] param = row.Split(',');
+                int paramLength = param.Length;
+
+                //Set Employee ID 
+                empId = param[0];
+
+                //One employee does not report to more than one manager.
+                if (paramLength > 3)
+                {
+                    throw new Exception("Employee reporting to more than one manager");
+                }
+
+                //Validate salary to be integers
+                salary = validateSalary(param[2]);
+
+                //There is only one CEO, i.e. only one employee with no manager.
+                if (ceoCount > 1)
+                {
+                    throw new Exception("There should only be one CEO");
+                }
+                else if (param[1] == "" && ceoCount <= 2)
+                {
+                    ceoCount++;
+                    managerId = param[1];
+                }
+                else
+                {
+                    managerId = param[1];
+                }
+                //Adding managers to managers dictionary
+                if (managerId != "")
+                {
+                    addManager(managerId, empId);
+                }
+
+                //foreach (string man in managers.Keys)
+                //{
+                //    if (!employees.ContainsKey(man))
+                //    {
+                //        throw new Exception("All managers should be employees");
+                //    }
+                //}
+                //Adding employees to employees dictionary
+                employees.Add(empId, createEmployee(empId, managerId, salary));
+
+                result = "Success";
+            }
+            return result;
+        }
+
+        //Validate salary to be integers
+        public int validateSalary(String salaryString)
+        {
+            int salary;
+            try
+            {
+                salary = int.Parse(salaryString);
+                return salary;
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException(e.Message + "Salaries need to be integers");
+            }
+        }
+
+        //One employee does not report to more than one manager.
+
+        //Create new employees
+        public Employee createEmployee(String emplID, String manID, int sal)
+        {
+            Employee employee = new Employee();
+            employee.empId = emplID;
+            employee.managerId = manID;
+            employee.salary = sal;
+            return employee;
+        }
+
+        //Add manager to the manager dictionary
+        public void addManager(String managerID, String employeeID)
+        {
+            if (managers.ContainsKey(managerID))
+            {
+                managers[managerID].Add(employeeID);
+            }
+            else
+            {
+                List<string> junior = new List<string>() { employeeID };
+                managers.Add(managerID, junior);
+            }
+        }
+
+        //Method to calculate budget for each manager
+        public long managerBudget(String employeeID)
+        {
+            long budget = 0;
+            //employee who is not a manager
+            if (!managers.ContainsKey(employeeID))
+            {
+                //Base case
+                budget = employees[employeeID].salary;
+            }
+            else
+            {
+                //Recurse 
+                foreach (string manager in managers[employeeID])
+                {
+                    budget += managerBudget(manager);
+                }
+                budget += employees[employeeID].salary;
+            }
+            return budget;
+        }
+
+    }
+}
---
+++ EmployeesTests/EmployeeTests.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.Tests
+{
+    [TestClass()]
+    public class EmployeeTests
+    {
+        Employee employees = new Employee();
+        StringBuilder csvString = new StringBuilder();
+
+        //Tests for Successfully reading the csv file
+        [TestMethod()]
+        public void readCsvTest()
+        {
+            string expected = "Success";
+            string actual = "";
+                
+            csvString.AppendLine("Employee4,Employee2,500");
+            csvString.AppendLine("Employee3,Employee1,500");
+            csvString.AppendLine("Employee1,,1000");
+            csvString.AppendLine("Employee5,Employee1,500");
+            csvString.AppendLine("Employee2,Employee1,800");
+
+            actual = employees.validateCsv(csvString.ToString());
+            Assert.AreEqual(expected, actual);
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void validSalaryTest()
+        {
+            csvString.AppendLine("Employee4,Employee2,500");
+            csvString.AppendLine("Employee3,Employee1,500");
+            csvString.AppendLine("Employee1,,1000");
+            csvString.AppendLine("Employee5,Employee1,500");
+            csvString.AppendLine("Employee2,Employee1,800");
+
+            employees = new Employee(csvString.ToString());
+        }
+
+        //Tests to validate salary to be integers
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void invalidSalaryTest()
+        {
+            csvString.AppendLine("Employee4,Employee2,500");
+            csvString.AppendLine("Employee3,Employee1,500");
+            csvString.AppendLine("Employee1,,1000");
+            csvString.AppendLine("Employee5,Employee1,yui");
+            csvString.AppendLine("Employee2,Employee1,800");
+
+            employees = new Employee(csvString.ToString());
+        }
+
+        // Tests to validate one employee does not report to more than one manager.
+        [TestMethod()]
+        [ExpectedException(typeof(Exception))]
+        public void oneManagerPerEmployeeTest()
+        {
+            csvString.AppendLine("Employee4,Employee2,500");
+            csvString.AppendLine("Employee3,Employee1,Employee2,500");
+            csvString.AppendLine("Employee1,,1000");
+            csvString.AppendLine("Employee5,Employee1,500");
+            csvString.AppendLine("Employee2,Employee1,800");
+
+            employees = new Employee(csvString.ToString());
+            //Assert.Fail();
+        }
+
+        //There is only one CEO, i.e. only one employee with no manager.
+        [TestMethod()]
+        [ExpectedException(typeof(Exception))]
+        public void oneCEOTest()
+        {
+            csvString.AppendLine("Employee4,Employee2,500");
+            csvString.AppendLine("Employee3,Employee1,500");
+            csvString.AppendLine("Employee1,,1000");
+            csvString.AppendLine("Employee5,,500");
+            csvString.AppendLine("Employee2,Employee1,800");
+
+            employees = new Employee(csvString.ToString());
+            //Assert.Fail();
+        }
+
+        //Test to Validate all managers to be employees
+        [TestMethod()]
+        [ExpectedException(typeof(Exception))]
+        public void managerIsEmployeeTest()
+        {
+            csvString.AppendLine("Employee4,Employee6,500");
+            csvString.AppendLine("Employee3,Employee1,500");
+            csvString.AppendLine("Employee1,,1000");
+            csvString.AppendLine("Employee5,Employee1,500");
+            csvString.AppendLine("Employee2,Employee1,800");
+
+            employees = new Employee(csvString.ToString());
+            //Assert.Fail();
+        }
+
+        //Test for budget for a manager who is the CEO
+        [TestMethod()]
+        public void ceoBudgetTest()
+        {
+            long expected = 3300;
+            long actual = 0;
+            csvString.AppendLine("Employee4,Employee2,500");
+            csvString.AppendLine("Employee3,Employee1,500");
+            csvString.AppendLine("Employee1,,1000");
+            csvString.AppendLine("Employee5,Employee1,500");
+            csvString.AppendLine("Employee2,Employee1,800");
+
+            employees = new Employee(csvString.ToString());
+
+            actual = employees.managerBudget("Employee1");
+            Assert.AreEqual(expected, actual);
+            //Assert.Fail();
+        }
+
+        //Test for a budget of a specific manager
+        [TestMethod()]
+        public void managerBudgetTest()
+        {
+            long expected = 1300;
+            long actual = 0;
+            csvString.AppendLine("Employee4,Employee2,500");
+            csvString.AppendLine("Employee3,Employee1,500");
+            csvString.AppendLine("Employee1,,1000");
+            csvString.AppendLine("Employee5,Employee1,500");
+            csvString.AppendLine("Employee2,Employee1,800");
+
+            employees = new Employee(csvString.ToString());
+
+            actual = employees.managerBudget("Employee2");
+            Assert.AreEqual(expected, actual);
+            //Assert.Fail();
+        }
+
+        //Test for an employee who is not a manager
+        [TestMethod()]
+        public void juniourBudgetTest()
+        {
+            long expected = 500;
+            long actual = 0;
+            csvString.AppendLine("Employee4,Employee2,500");
+            csvString.AppendLine("Employee3,Employee1,500");
+            csvString.AppendLine("Employee1,,1000");
+            csvString.AppendLine("Employee5,Employee1,500");
+            csvString.AppendLine("Employee2,Employee1,800");
+
+            employees = new Employee(csvString.ToString());
+
+            actual = employees.managerBudget("Employee5");
+            Assert.AreEqual(expected, actual);
+            //Assert.Fail();
+        }
+    }
+}
